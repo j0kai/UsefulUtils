9d73bd2b4f7b56ab6c41420111dbdb99f0ecf0e5
Kyle Jones <105503076+j0kai@users.noreply.github.com>
2024-10-22 14:22:35 +0100

    Created serializable structs for Guid, vector3 and quaternion

---
+++ Runtime/Serialization/SerializableVector3.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System;
+
+/// <summary>
+/// Represents a serializable version of the Unity Vector3 struct.
+/// </summary>
+
+[Serializable]
+public struct SerializableVector3
+{
+    public float x;
+    public float y;
+    public float z;
+
+    public SerializableVector3(float x, float y, float z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public override string ToString() => $"[{x}, {y}, {z}]";
+
+    public static implicit operator Vector3(SerializableVector3 vector)
+    {
+        return new Vector3(vector.x, vector.y, vector.z);
+    }
+
+    public static implicit operator SerializableVector3(Vector3 vector)
+    {
+        return new SerializableVector3(vector.x, vector.y, vector.z);
+    }
+}
