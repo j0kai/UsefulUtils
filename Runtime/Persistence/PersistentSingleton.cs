c4f0b75cce4b5974ab74171d226525b6f8a82da0
Kyle Jones <105503076+j0kai@users.noreply.github.com>
2025-01-06 15:28:55 +0000

    update: tracking whether application is quitting to ensure proper deletion of the object

---
+++ Runtime/Persistence/PersistentSingleton.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class PersistentSingleton<T> : MonoBehaviour where T : Component
+{
+    [Tooltip("If this is true, this singleton will automatically be placed on the root level of the scene hierarchy.")]
+    public bool AutoUnparentOnAwake = true;
+
+    // Tracks whether the application is shutting down.
+    protected static bool IsApplicationQuitting = false;
+
+    public static bool HasInstance => s_Instance != null;
+    public static T Current => s_Instance;
+
+    protected static T s_Instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (s_Instance == null)
+            {
+                s_Instance = FindObjectOfType<T>();
+                if (s_Instance == null && !IsApplicationQuitting)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(T).Name + " [Auto Generated]";
+                    s_Instance = obj.AddComponent<T>();
+                }
+            }
+
+            return s_Instance;
+        }
+    }
+
+    protected virtual void Awake() => InitializeSingleton();
+
+    protected virtual void InitializeSingleton()
+    {
+        if (!Application.isPlaying)
+            return;
+
+        if (AutoUnparentOnAwake)
+            transform.SetParent(null);
+
+        if (s_Instance == null)
+        {
+            s_Instance = this as T;
+            DontDestroyOnLoad(transform.gameObject);
+            enabled = true;
+        }
+        else
+        {
+            if (this != s_Instance)
+                Destroy(gameObject);
+        }
+    }
+
+    protected virtual void OnApplicationQuit()
+    {
+        IsApplicationQuitting = true;
+
+        s_Instance = null;
+
+        if(Application.isEditor)
+            DestroyImmediate(gameObject);
+        else
+            Destroy(gameObject);
+    }
+
+}
+
